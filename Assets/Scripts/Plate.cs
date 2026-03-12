27512be5d69e12ccad7d2d31de13d92ded827c4f
Basiltastic <sundaygmer@gmail.com>
2026-03-12 13:02:02 -0400

    plate update

--- Assets/Scripts/Plate.cs
+++ Assets/Scripts/Plate.cs
@@ -3,30 +3,30 @@ using UnityEngine;
 using System.Collections.Generic;
 public class Plate : MonoBehaviour
 {
-    public GameObject breakfastFood;
     public List<GameObject> breakfastFoodList = new List<GameObject>();
     public bool foodOnPlate = false;
 
-    //private void OnTriggerEnter(Collider other)
-    //{
-    //    if (other.CompareTag("Food"))
-    //    {
-    //        if (!foodOnPlate.Contains(other.gameObject))
-    //        {
-    //            foodOnPlate.Add(other.gameObject);
-    //            foodOnPlate = true;
-    //            Debug.Log(other.name + "hasfoodlol");
-    //        }
-    //    }
-    //}
-    //private void OnTriggerExit(Collider other)
-    //{
-    //    if (other.CompareTag("Food"))
-    //    {
-    //        foodOnPlate = false;
-    //        Debug.Log("hasnofoodlol");
-    //    }
-    //}
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Food"))
+        {
+            if (!breakfastFoodList.Contains(other.gameObject))
+            {
+                breakfastFoodList.Add(other.gameObject);
+                foodOnPlate = true;
+                Debug.Log(other.name + "hasfoodlol");
+            }
+        }
+    }
+    private void OnTriggerExit2D(Collider2D other)
+    {
+        if (breakfastFoodList.Contains(other.gameObject))
+        {
+            breakfastFoodList.Remove(other.gameObject);
+            foodOnPlate = false;
+            Debug.Log("hasnofoodlol" + other.gameObject);
+        }
+    }
 
     void Update()
     {
